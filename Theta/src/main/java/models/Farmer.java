b14b462e36108d1a180d0791040a8d0be9de02f0
kajela <titoyut56@gmail.com>
2023-06-30 11:15:27 +0300

    add the initial files

---
+++ Theta/src/main/java/models/Farmer.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Farmer {
+    private String name;
+    private  String ktdaNo;
+    private String phoneNo;
+    private  String email;
+    private  int id;
+
+    public Farmer(String name, String ktdaNo, String phoneNo, String email) {
+        this.name = name;
+        this.ktdaNo = ktdaNo;
+        this.phoneNo = phoneNo;
+        this.email = email;
+    }
+}
