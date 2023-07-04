c6dc5b8ba8d18af85ad001dbc5ca271faec9f999
kajela <titoyut56@gmail.com>
2023-07-04 17:57:43 +0300

    add the initial files

---
+++ portal/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <Navbar />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ portal/src/NavBar.js
@@ -0,0 +1,18 @@
+const Navbar = () => {
+    return (
+      <nav>
+            <a href="/">
+            <button>Home</button>
+                </a>
+            <a href="/about">
+                <button>About</button>
+                </a>
+            <a href="/contact">
+                <button>Contact</button>
+                </a>
+      </nav>
+    );
+  };
+  
+  export default Navbar;
+  
